9d58499765b80685ed8cf397e941772345780dba
Samuel <60717097+Shamsam@users.noreply.github.com>
2025-11-02 14:52:22 -0500

    feat/create-primitive-map

---
+++ StealYoBike/src/routes/dashboard/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Map from '$lib/components/Map/Map.svelte';
+</script>
+
+<div class="relative top-0 h-20 w-full bg-red-300"></div>
+<div class="relative flex h-[calc(100%-5rem)] w-full bg-blue-300">
+	<div class="h-[50%] w-[50%] overflow-hidden rounded-2xl *:size-full">
+		<Map />
+	</div>
+</div>
+<div class="h-40 w-full"></div>
